5bf157c6b9c22231cbd856d0572b91cf4b2aba26
huynhnhule2004 <huynhnhule2004@gmail.com>
2024-11-21 14:17:07 +0700

    Init source

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './layout/Sidebar.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import Header from './layout/Header.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    <Sidebar></Sidebar>
+    <Header></Header>
+  </div>
+
+  <!-- <HelloWorld/> -->
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+fgdgd
+</template>
+
+<style scoped>
+
+</style>
